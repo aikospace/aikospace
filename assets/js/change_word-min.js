9bea7e225f73a130ad9201e9c3cab4160899040d
Lorenzo Feruglio <lorenzo.feruglio@gmail.com>
2017-05-24 13:28:43 +0200

    updated. Added Partners and Facebook page

--- assets/js/change_word-min.js
+++ assets/js/change_word-min.js
@@ -12,7 +12,7 @@ initialTimer = setTimeout(function asd() {
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "z", "!", "£", "$", "%", "&", "/", "(", ")", "=", "?", "^", "é", "è", "+", "*", "à", "ò", "ù", "-", ".", ",", "°", "§", "ç", "{", "}"];
 var final_name = [String.fromCharCode(34 + 45), String.fromCharCode(2 * 8 / 2 / 4 * 41), String.fromCharCode(69 - 2), String.fromCharCode(2 * 3 * 6 * 2), String.fromCharCode(138 / 2), String.fromCharCode(100 - 17), String.fromCharCode(84), String.fromCharCode(82), String.fromCharCode(65)];
 var r_text = new Array();
-r_text = ["ONES", "OFFICE", "OPERATORS", "ORGANIZATION", "OBSESSED", "ORIGAMI", "OH YEAH!", "OBJECTS", "OFFICERS", "OAKS", "OATMEALS", "OCARINAS", "OWLS", "OPTIMIZATION", "OPPORTUNITY", "OPENERS", "OMELETTES", "ODYSSEY"];
+r_text = ["ONES", "OFFICE", "OPERATORS", "ORGANIZATION", "ORIGAMI", "OH YEAH!", "OBJECTS", "OFFICERS", "OAKS", "OATMEALS", "OCARINAS", "OWLS", "OPTIMIZATION", "OPPORTUNITY", "OPENERS", "OMELETTES", "ODYSSEY"];
 var r2_text = new Array();
 var idx = 0;
 var idx_array = new Array();
@@ -22,24 +22,31 @@ for (idx = 0; idx < r_text.length - 1; idx++) {
 }
 shuffle(idx_array);
 var i = 0;
-i = Math.floor(100 * Math.random());
+i = Math.floor(20 * Math.random());
 
 function pickChange() {
     clearInterval(handler);
     handler = setInterval(pickChange, interval);
-    if (i > 0 && i < 5 && count < final_value) {
+    if (i > 0 && i < 5 && count <= final_value) {
         changeText()
-    } else {
-        if (i == 0 && count < final_value) {
-            changeText2()
-        }
+    }
+    if (i > 4 && i < 10 && count < final_value) {
+        changeText2()
+    }
+    if (i > 9 && i < 15 && count < final_value) {
+        changeText3()
     }
 }
 
 function changeText() {
     count2 < total_words ? count = idx_array[count2] : count = final_value;
-    $("#INITIAL-NAME").text(initial_name);
-    $("#O-WORD").text(r_text[idx_array[count]]);
+    $("#INITIAL-NAME").text(initial_name)
+    if (count==final_value) {
+      $("#O-WORD").text(final_name.join(""));
+    } else {
+      $("#O-WORD").text(r_text[idx_array[count]]);
+    }
+
     count2 < total_words ? count2++ : count2 = total_words;
     interval = interval + count * 8
 }
@@ -69,6 +76,11 @@ function changeText2() {
     if (counter == final_name.length) {
         clearInterval(handler)
     }
+}
+
+function changeText3() {
+  $("#INITIAL-NAME").text("ARTIFICIAL INTELLIGENCE IN SPACE. ")
+  $("#O-WORD").text("DELIVERED.")
 }
 
 function shuffle(d) {
